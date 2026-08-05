7814b80acccd4fdbb890d9ffb48f1b5cdca60a35
limilyt <allat9244@gmail.com>
2026-08-05 09:28:40 +0300

    Update download.py

--- download.py
+++ download.py
@@ -1,1 +1,10 @@
+import requests
 
+URL = "https://services.swpc.noaa.gov/text/3-day-forecast.txt"
+
+r = requests.get(URL, timeout=30)
+print("HTTP:", r.status_code)
+print("Length:", len(r.text))
+
+with open("forecast.txt", "w", encoding="utf-8") as f:
+    f.write(r.text)
