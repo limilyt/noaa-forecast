971281ef1e4d42020f09eba7e92ab384e414ccca
limilyt <allat9244@gmail.com>
2026-08-05 10:29:47 +0300

    Update download.py

--- download.py
+++ download.py
@@ -8,3 +8,12 @@ print("Length:", len(r.text))
 
 with open("forecast.txt", "w", encoding="utf-8") as f:
     f.write(r.text)
+
+URL = "https://services.swpc.noaa.gov/text/27-day-outlook.txt"
+
+r = requests.get(URL, timeout=30)
+print("HTTP:", r.status_code)
+print("Length:", len(r.text))
+
+with open("forecast1.txt", "w", encoding="utf-8") as f:
+    f.write(r.text)
